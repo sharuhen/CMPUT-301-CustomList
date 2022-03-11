97f1264c7d564a222b0f07fb2b43d989f116f5e7
sharuhen <60045219+sharuhen@users.noreply.github.com>
2022-03-11 11:27:31 -0700

    tes

---
+++ app/src/test/java/CustomListTest.java
@@ -0,0 +1,24 @@
+import com.example.simpleparadox.listycity.City;
+import com.example.simpleparadox.listycity.CustomList;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import java.util.ArrayList;
+
+public class CustomListTest {
+    private CustomList list;
+
+    @Before
+    public void createList() {
+        list=new CustomList(null, new ArrayList<City>());
+    }
+
+    @Test
+    public void addCityTest() {
+        int listSize = list.getCount();
+        list.addCity(new City("Edmonton","AB"));
+        assertEquals(list.getCount(),listSize+1);
+    }
+}
